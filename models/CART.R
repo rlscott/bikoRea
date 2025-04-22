ed09871fdacbe3229d01b545badab61d85e9dc1b
unknown <lheming@iastate.edu>
2025-04-22 10:39:01 -0500

    Re-rean rf

--- models/CART.R
+++ models/CART.R
@@ -5,11 +5,11 @@ library(tictoc)
 library(tidyverse)
 set.seed(7)
 tic()
-CART10F<-train(y=bike_train$rented_bikes, x=bike_train[,2:16], method="rpart",
+CART10F<-train(y=bike_train$rented_bikes, x=bike_train[,2:15], method="rpart",
                     tuneGrid=data.frame(cp=(1:100)/100000),
                     trControl=trainControl(method="repeatedcv",
-                                           repeats = 100,number=10))
-toc() #566.86 seconds
+                                           repeats = 10,number=10))
+toc() # 80.09 seconds
 
 CART10F
 CART10F_plot <- ggplot(CART10F) + labs(title = "CART 10-Fold CV", y = "RMSE") + 
@@ -21,4 +21,4 @@ ggsave("figures/CART_10FCV.pdf", CART10F_plot, width = 7, height = 5, dpi = 300)
 
 CART_test_pred <- predict(CART10F, bike_test)
 CART_test_rmse <- RMSE(CART_test_pred, bike_test$rented_bikes)
-CART_test_rmse # 278.8306
+CART_test_rmse # 285.9258
