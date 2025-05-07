762543f5cf4745020dfc967fc407160545bf2b4a
unknown <lheming@iastate.edu>
2025-05-07 13:56:32 -0500

    stacked updated

--- models/StackedModel.R
+++ models/StackedModel.R
@@ -1,13 +1,12 @@
 load("data/top5Pred.Rdata")
 load("data/top5Pred_test.Rdata")
-load("data/yhats_BART.Rdata")
 library(caret)
 library(tictoc)
 library(tidyverse)
 library(randomForest)
 library(xtable)
 
-preddf_test<-data.frame(top5Pred_test,BART=yhats_BART_test)
+preddf_test<-data.frame(top5Pred_test)
 preddf<-data.frame(top5Pred[,-2])
 
 set.seed(777)
@@ -15,10 +14,10 @@ tic()
 stacked<-train(y=preddf$y, x=preddf[,2:6], method="rf",
               tuneGrid=data.frame(.mtry=1:5),importance=T,
               trControl=trainControl(method="oob"),nodesize=5) # m OOB selected, nmin=5
-toc() # 642.35 seconds
+toc() # 
 
 stacked # RMSE = 23.89370  
 
 RF_test_pred <- predict(stacked, preddf_test)
-RF_test_rmse <- RMSE(RF_test_pred, bike_test$y)
+RF_test_rmse <- RMSE(RF_test_pred, preddf_test$y)
 RF_test_rmse # 
